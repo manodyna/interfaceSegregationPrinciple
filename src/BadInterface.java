2718237e6b74738cbe5c305442fdea266f7c59da
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2022-04-23 22:57:20 +0530

    Update BadInterface.java

--- src/BadInterface.java
+++ src/BadInterface.java
@@ -1,5 +1,6 @@
 //This is an example of a bad interface when we add the loanPayment method to it,
-// all classes implementing this interface are now forced to implement the loanPayment method now causing complications.
+// all classes implementing this interface are now forced to implement 
+// the loanPayment method now causing complications.
 public interface BadInterface {
     public void initiatePayment(String customerId);
     public void disburseLoan(String customerId, int amount);
